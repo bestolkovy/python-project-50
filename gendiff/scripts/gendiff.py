e771478dfff42de2dd3cc7b81bf1f34b2bb9f27a
vito <vbykov@inbox.ru>
2024-06-24 01:54:23 +0700

    step1, help added

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    args = parser.parse_args()
+    print(f'{args.first_file} fucks {args.second_file}')
+
+
+if __name__ == '__main__':
+    main()
