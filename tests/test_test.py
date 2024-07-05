6b4ff9999f5cb36def1ae24cb5ef179c52e281c5
vito <vbykov@inbox.ru>
2024-07-05 19:39:15 +0700

    outsource file test1 added

--- tests/test_test.py
+++ tests/test_test.py
@@ -5,11 +5,4 @@ import json
 def test_compare():
     file1 = json.load(open('./tests/fixtures/file1.json'))
     file2 = json.load(open('./tests/fixtures/file2.json'))
-    assert generate_diff(file1, file2) == '''{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}'''
+    assert generate_diff(file1, file2) == open('./tests/fixtures/test1.txt').read() # noqa
