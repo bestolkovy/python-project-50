3913a91d4dc7f14cdce99f77b3fb480fd63d0777
vito <vbykov@inbox.ru>
2024-07-06 16:59:39 +0700

    n4

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,8 +1,14 @@
-from gendiff import generate_diff
+from gendiff import generate_diff, parsing
 import json
+import yaml
 
 
 def test_compare():
     file1 = json.load(open('./tests/fixtures/file1.json'))
     file2 = json.load(open('./tests/fixtures/file2.json'))
     assert generate_diff(file1, file2) == open('./tests/fixtures/test1.txt').read() # noqa
+
+
+def test_parsing():
+    assert parsing(json.load(open('./tests/fixtures/file1.json'))) == {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
+    assert parsing(yaml.safe_load(open('./tests/fixtures/file2.yml'))) == {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'}
