e05245177bb4e5ea1ca1400d6261eded1a9babe2
bestolkovy <f326ehm@gmail.com>
2024-11-03 21:31:03 +0700

    остались пустые строки

--- gendiff/stuff/gendiff_mod.py
+++ gendiff/stuff/gendiff_mod.py
@@ -1,20 +1,20 @@
-def dict_to_string(d, indent=3):
+def dict_to_string(d, depth):
     result = ""
-    indent_space = ".." * indent
+    indent_space = "    " * (depth+1)
     result += "{\n"
     for key, value in d.items():
-        result += f"{indent_space}  {key}: "
+        result += f"{indent_space}    {key}: "
         if isinstance(value, dict):
-            result += dict_to_string(value, indent + 1)
+            result += dict_to_string(value, depth + 1)
         else:
             result += f"{value}\n"
     result += f"{indent_space}}}\n"
     return result
 
 
-def format_value(value):
+def format_value(value, depth):
     if isinstance(value, dict):
-        formatted = dict_to_string(value)
+        formatted = dict_to_string(value, depth)
         return formatted
     else:
         return str(value)
@@ -26,21 +26,20 @@ def format_data(data):
         status = item['status']
         key = item['key']
         depth = item['depth']
-        indent = ',,' * depth
+        indent = '    ' * depth
         # Проверяем, есть ли ключ 'value' в элементе
         value = item.get('value', None)
         if status == 'nested' and isinstance(value, list):
-            result += f'{indent}----{key}: {format_data(value)}\n'
+            result += f'{indent}    {key}: {format_data(value)}\n'
         else:
             if status == 'added':
-                result += f'{indent}  + {key}: {format_value(value)}\n'
+                result += f'{indent}  + {key}: {format_value(value, depth)}\n'
             elif status == 'deleted':
-                result += f'{indent}  - {key}: {format_value(value)}\n'
+                result += f'{indent}  - {key}: {format_value(value, depth)}\n'
             elif status == 'changed':
-                result += f'{indent}  - {key}: {format_value(item["value_old"])}\n'
-                result += f'{indent}  + {key}: {format_value(item["value_new"])}\n'
+                result += f'{indent}  - {key}: {format_value(item["value_old"], depth)}\n'
+                result += f'{indent}  + {key}: {format_value(item["value_new"], depth)}\n'
             else:
-                result += f'{indent}===={key}: {format_value(value)}\n'
-    result += '\\' * (depth - 1) + '}'
+                result += f'{indent}    {key}: {format_value(value, depth)}\n'
+    result += '    ' * (depth) + '}'
     return result
-
